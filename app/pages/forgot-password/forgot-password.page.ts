c83e975248f132b9bc2436a6566810ad4f8b62f3
ManuACDev <malamocres@gmail.com>
2023-10-14 13:21:59 +0200

    Funcionalidad Reset Password

--- app/pages/forgot-password/forgot-password.page.ts
+++ app/pages/forgot-password/forgot-password.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,9 +12,20 @@ export class ForgotPasswordPage implements OnInit {
 
   correo = null;
 
-  constructor() { }
+  constructor(private auth: AuthService, private toast: InteractionService, private router:Router) { }
 
   ngOnInit() {
+  }
+  
+  async onReset() {
+    try {
+      const correo = this.correo;
+      await this.auth.resetPassword(correo);
+      this.toast.presentToast("Correo enviado. Consulta su bandeja de entrada", 1500);
+      this.router.navigate(['login']);
+    } catch (error) {
+      this.toast.presentToast("Error al enviar correo. Verifica la dirección.", 1500);
+    }
   }
 
 }
