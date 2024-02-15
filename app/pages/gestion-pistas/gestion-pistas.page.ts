40ac42693341f539a225d02eccd1d1e83cc4bc99
ManuACDev <malamocres@gmail.com>
2024-02-15 20:12:25 +0100

    Cambio Recuperar Pistas

--- app/pages/gestion-pistas/gestion-pistas.page.ts
+++ app/pages/gestion-pistas/gestion-pistas.page.ts
@@ -45,9 +45,7 @@ export class GestionPistasPage implements OnInit {
     const path = `Pistas`;
     const pistas = await this.firestore.getCollection<Pista>(path);
     pistas.subscribe(data => {
-      data.forEach((doc) => {
-        this.pistas.push(doc);
-      });
+      this.pistas = data;
     });
   }
 
