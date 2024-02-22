13206db8380e883a19a9999f04566ef546ce5767
ManuACDev <malamocres@gmail.com>
2024-02-22 21:31:58 +0100

    ActionSheet Editar Usuario

--- app/pages/editar-usuario/editar-usuario.page.ts
+++ app/pages/editar-usuario/editar-usuario.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { ActionSheetController, MenuController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { FirestoreService } from 'src/app/services/firestore.service';
@@ -15,7 +15,7 @@ export class EditarUsuarioPage implements OnInit {
   usuario: string = null;
   public usuario$: Observable<User>;
 
-  constructor(private menuCtrl: MenuController, private route: ActivatedRoute, private firestore: FirestoreService) { }
+  constructor(private menuCtrl: MenuController, private route: ActivatedRoute, private firestore: FirestoreService, private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -31,6 +31,41 @@ export class EditarUsuarioPage implements OnInit {
   async obtenerUsuario() {
     const path = `Usuarios`;
     this.usuario$ = this.firestore.getDoc<User>(path, this.usuario);
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          handler: () => {
+            console.log('Borrar Cuenta clicked');
+          }
+        },
+        {
+          text: 'Inhabilitar',
+          handler: () => {
+            console.log('Inhabilitar');
+          }
+        },
+        {
+          text: 'Restablecer Contraseña',
+          handler: () => {
+            console.log('Restablecer');
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+  
+    await actionSheet.present();
   }
 
 }
