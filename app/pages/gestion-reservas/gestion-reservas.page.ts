f892227580f778c0074d2d17939f2214a7ddc95c
ManuACDev <malamocres@gmail.com>
2024-03-09 18:18:51 +0100

    Menú Auto Reservas

--- app/pages/gestion-reservas/gestion-reservas.page.ts
+++ app/pages/gestion-reservas/gestion-reservas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-gestion-reservas',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionReservasPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidLeave() {
+    this.menuCtrl.close();
   }
 
 }
