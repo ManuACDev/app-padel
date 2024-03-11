35c0527c3603c4e6866ddbdd5ffba6953864ef23
ManuACDev <malamocres@gmail.com>
2024-03-11 11:25:44 +0100

    Display Modo Usuarios

--- app/pages/gestion-reservas/gestion-reservas.page.ts
+++ app/pages/gestion-reservas/gestion-reservas.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Pista } from 'src/app/models/pista.model';
 import { Reserva } from 'src/app/models/reserva.model';
+import { User } from 'src/app/models/user.model';
 import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
@@ -11,10 +12,15 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 })
 export class GestionReservasPage implements OnInit {
 
+  usuarios: User[] = [];
   pistas: Pista[] = [];
   reservas: Reserva[] = [];
+
   results: Reserva[] = [];
+
   reservasPistas: { [pistaId: string]: Reserva[] } = {};
+  reservasUsuarios: { [usuarioId: string]: Reserva[] } = {};
+
   filtroPistas: boolean = false;
   filtroUsuarios: boolean = false;
   filtroReservas: boolean = true;
@@ -22,14 +28,30 @@ export class GestionReservasPage implements OnInit {
   constructor(private menuCtrl: MenuController, private firestore: FirestoreService) { }
 
   ngOnInit() {
-    this.obtenerPistas();
+    this.obtenerUsuarios();
+    setTimeout(() => {
+      this.obtenerPistas();
+    }, 500);
     setTimeout(() => {
       this.obtenerReservas();
+    }, 600);
+    setTimeout(() => {
+      this.obtenerReservasUsuarios();
     }, 600);
   }
 
   ionViewDidLeave() {
     this.menuCtrl.close();
+  }
+
+  async obtenerUsuarios() {
+    this.usuarios = [];
+
+    const path = `Usuarios`;
+    const usuarios = await this.firestore.getCollection<User>(path);
+    usuarios.subscribe(data => {
+      this.usuarios = data;
+    });
   }
 
   async obtenerPistas() {
@@ -61,17 +83,40 @@ export class GestionReservasPage implements OnInit {
     }
   }
 
+  async obtenerReservasUsuarios() {
+    
+    for (const usuario of this.usuarios) {
+      const id = usuario.uid;
+
+      for (const pista of this.pistas) {
+        const path = `Pistas/${pista.id}/Reservas`;
+        const reservas = await this.firestore.getCollectionId<Reserva>(id, path);
+
+        this.reservasUsuarios[usuario.uid] = [];
+
+        reservas.subscribe(data => {
+          data.forEach((doc) => {
+            const reserva = doc.data() as Reserva;
+            this.reservasUsuarios[usuario.uid].push(reserva);
+          });
+        });
+      }
+    }
+  }
+
   searchReserva(event) {
     const query = event.target.value;
     if (query.trim() !== '') {
-      this.results = this.reservas.filter(reserva => {
-        return Object.values(reserva).some(value => {
-          if (typeof value === 'string') {
-            return value.toLowerCase().includes(query);
-          }
-          return false;
+      if (this.filtroReservas) {
+        this.results = this.reservas.filter(reserva => {
+          return Object.values(reserva).some(value => {
+            if (typeof value === 'string') {
+              return value.toLowerCase().includes(query);
+            }
+            return false;
+          });
         });
-      });
+      }
     } else {
       this.results = this.reservas;
     }    
