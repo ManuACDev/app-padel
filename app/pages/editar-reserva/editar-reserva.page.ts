332417bbdd905314e64b250045a0cf33859ed6cc
ManuACDev <malamocres@gmail.com>
2024-03-17 20:27:33 +0100

    Cancelar Reserva

--- app/pages/editar-reserva/editar-reserva.page.ts
+++ app/pages/editar-reserva/editar-reserva.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 import { Pista } from 'src/app/models/pista.model';
 import { Reserva } from 'src/app/models/reserva.model';
 import { User } from 'src/app/models/user.model';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-editar-reserva',
@@ -18,7 +20,7 @@ export class EditarReservaPage implements OnInit {
   reserva: Reserva = null;
   pistas: Pista[] = [];
 
-  constructor(private route: ActivatedRoute, private firestore: FirestoreService) { }
+  constructor(private route: ActivatedRoute, private firestore: FirestoreService, private actionSheetCtrl: ActionSheetController, private toast: InteractionService, private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -66,4 +68,54 @@ export class EditarReservaPage implements OnInit {
     }
   }
 
+  async presentActionSheet(reserva: Reserva) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          handler: () => {
+            this.borrarReserva(reserva.pista, reserva);
+          }
+        },
+        {
+          text: 'Reembolsar',
+          handler: () => {
+            //this.changePassword(reserva);
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+  
+    await actionSheet.present();
+  }
+  
+  async borrarReserva(pista: string, reserva: Reserva) {
+    const id = reserva.id;
+    const path = `Pistas/${pista}/Reservas`;
+
+    console.log(path + " " + id);
+
+    await this.firestore.deleteDoc<Reserva>(id, path).then(() => {
+      this.navegarComponente('gestion-usuarios');
+      this.toast.presentToast("Reserva cancelada correctamente.", 1000);
+    }).catch(error => {
+      console.error(error);
+      this.toast.presentToast("Error al cancelar la reserva", 1000);
+    });
+  }
+
+  async navegarComponente(componente: string) {
+    this.toast.presentToast("Cargando...", 500);
+    setTimeout(() => {
+      this.router.navigate(['/',componente]);
+    }, 500);
+  }
 }
