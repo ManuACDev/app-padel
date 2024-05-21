064a4406fb2d320730d4b7dc1db272d477407f84
ManuACDev <malamocres@gmail.com>
2024-05-21 23:20:21 +0200

    Reembolsar Pago Reserva Duplicada

--- app/pages/editar-duplicados/editar-duplicados.page.ts
+++ app/pages/editar-duplicados/editar-duplicados.page.ts
@@ -1,10 +1,14 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ActionSheetController, LoadingController } from '@ionic/angular';
 import { Duplicado } from 'src/app/models/duplicado.model';
 import { Pago } from 'src/app/models/pago.model';
 import { Reserva } from 'src/app/models/reserva.model';
 import { User } from 'src/app/models/user.model';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import { InteractionService } from 'src/app/services/interaction.service';
+import { StripeService } from 'src/app/services/stripe.service';
 
 @Component({
   selector: 'app-editar-duplicados',
@@ -19,9 +23,9 @@ export class EditarDuplicadosPage {
   usuarios: User[] = [];
   reservasUsuarios: Reserva[] = [];
   pagos: Pago[] = [];
-  pagosUsuarios: Pago[] = [];
+  pagosReservas: Pago[] = [];
 
-  constructor(private route: ActivatedRoute, private firestore: FirestoreService) { }
+  constructor(private route: ActivatedRoute, private firestore: FirestoreService, private actionSheetCtrl: ActionSheetController, private toast: InteractionService, private router: Router, private loadingCtrl: LoadingController, private stripeService: StripeService) { }
 
   ionViewWillEnter () {
     this.route.queryParams.subscribe(params => {
@@ -32,7 +36,7 @@ export class EditarDuplicadosPage {
       this.obtenerReservasUsuarios();
     }, 250);
     setTimeout(() => {
-      this.obtenerPagosUsuarios();
+      this.obtenerPagosReservas();
     }, 400);
   }
 
@@ -68,25 +72,92 @@ export class EditarDuplicadosPage {
     }    
   }
 
-  async obtenerPagosUsuarios() {
-    const payDocs = this.duplicado.reservas.map(reserva => reserva.paymentDoc);
+  async obtenerPagosReservas() {
+    const payDocs = this.duplicado.reservas.map((reserva: Reserva) => reserva.paymentDoc);
+    const path = `Pagos`;
+    
+    const pagos = await this.firestore.getCollection<Pago>(path);
+    pagos.subscribe(data => {
+      const filteredPagos = data.filter(pago => payDocs.includes(pago.id));
+      for (const pago of filteredPagos) {
+        this.pagosReservas[pago.id] = pago;
+      }
+    });
+  }
 
-    for (const usuario of this.usuarios) {
-      const id = usuario.uid;
-      const path = 'Pagos';
-      const pagos = await this.firestore.getCollectionId<Pago>(id, path);
-
-      this.pagosUsuarios[usuario.uid] = [];
-
-      pagos.subscribe(data => {
-        data.forEach((doc) => {
-          const pago = doc.data() as Pago;
-          if (payDocs.includes(pago.id)) {
-            this.pagosUsuarios[usuario.uid].push(pago);
+  async presentActionSheet(reserva: Reserva, pago: Pago) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Reembolsar',
+          handler: () => {
+            this.reembolsarReserva(reserva, pago);
           }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+  
+    await actionSheet.present();
+  }
+
+  async reembolsarReserva(reserva: Reserva, pago: Pago) {
+    const loading = await this.showLoading('Reembolsando reserva...');
+    try {
+      const fecha = new Date();
+      const fechaFormateada = formatDate(fecha, 'dd/MM/yyyy', 'en-US');
+      
+      const response = await this.stripeService.refund(pago.paymentIntentId, reserva.uid, fechaFormateada);
+      
+      if (response == true) {
+        const id = reserva.id;
+        const path = `Pistas/${reserva.pista}/Reservas`;
+
+        await this.firestore.deleteDoc<Reserva>(path, id).then(async () => {
+          const path = `Pagos`;
+          const id = reserva.paymentDoc;
+
+          await this.firestore.deleteDoc<Pago>(path, id).then(() => {
+            this.navegarComponente('gestion-reservas');
+            this.toast.presentToast("Pago reembolsado correctamente", 1000);
+          }).catch(error => {
+            console.error(error);
+            this.toast.presentToast("Error al reembolsar el pago", 1000);
+          });
+        }).catch(error => {
+          console.error(error);
+          this.toast.presentToast("Error al reembolsar el pago", 1000);
         });
-      });
+      } else {
+        loading.dismiss();
+        this.toast.presentToast("Error al reembolsar el pago", 1000); 
+      }
+    } catch (error) {
+      console.error(error);
+      this.toast.presentToast("Error al reembolsar el pago", 1000);
+    } finally {
+      loading.dismiss();
     }
   }
 
+  async showLoading(mensaje: string) {
+    const loading = await this.loadingCtrl.create({
+      message: mensaje,
+    });
+    loading.present();
+    return loading;
+  }
+
+  async navegarComponente(componente: string) {
+    this.toast.presentToast("Cargando...", 500);
+    setTimeout(() => {
+      this.router.navigate(['/',componente]);
+    }, 500);
+  }
 }
